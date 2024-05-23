b3bb0cfc3ff5490bae7438add5765a452d7051af
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-05-23 13:20:12 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
@@ -87,10 +87,18 @@ INSERT INTO [Invoices]([Number], [IssueDate], [DueDate], [Amount], [Currency], [
 
 -- Problem 03
 
-	 UPDATE [Invoices]
-		SET [DueDate] = '2023-04-01'
+	   UPDATE [Invoices]
+		    SET [DueDate] = '2023-04-01'
  WHERE YEAR([IssueDate]) = 2022 AND MONTH([IssueDate]) = 11
 
 	 UPDATE [Clients]
-		SET [AddressId] = 3
+		  SET [AddressId] = 3
 	  WHERE [Name] LIKE '%CO%'
+
+-- Problem 05
+
+  SELECT [Number], 
+		     [Currency]
+	  FROM [Invoices]
+ORDER BY [Amount] DESC,
+		     [DueDate] 
