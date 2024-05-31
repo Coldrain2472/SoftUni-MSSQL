d6871f824a6ed6ac326927ec42f27c67a1343d4e
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-05-31 12:27:34 +0300

    Create 04.EmployeesFromTown.sql

---
+++ 07. Functions and Stored Procedures/04.EmployeesFromTown.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE [usp_GetEmployeesFromTown] @townName VARCHAR(50)
+			   AS
+			BEGIN
+						SELECT [FirstName] AS [First Name],
+							   [LastName] AS [Last Name]
+						  FROM [Employees] AS e
+						  JOIN [Addresses] AS a ON a.[AddressID] = e.[AddressID]
+						  JOIN [Towns] AS t ON t.[TownID] = a.[TownID]
+						 WHERE 
+							 t.[Name] = @townName
+			  END
