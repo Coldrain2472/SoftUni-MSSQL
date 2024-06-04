b909bada90f2494c20ba1e924079ab254465afb0
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-04 13:32:07 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Regular Exam - 15 Oct 2023/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Regular Exam - 15 Oct 2023/ExamSolutions.sql
@@ -109,3 +109,18 @@ SET Email = NULL
 ORDER BY 
 	   r.[Price] DESC,	
 	   b.[ArrivalDate]
+
+-- Problem 06
+  SELECT 
+	   h.[Id],
+	   h.[Name]
+    FROM [HotelsRooms] AS hr
+	JOIN [Hotels] AS h ON h.[Id] = hr.[HotelId]
+	JOIN [Rooms] AS r ON r.[Id] = hr.[RoomId]
+    JOIN [Bookings] AS b ON b.[HotelId] = h.[Id]
+   WHERE 
+	   r.[Type] = 'VIP Apartment'
+   GROUP BY 
+	   h.[Id], h.[Name]
+   ORDER BY
+		COUNT(b.[Id]) DESC
