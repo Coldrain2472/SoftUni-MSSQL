4ce14344319244cae729639e85e9e2748377254d
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-04 13:49:41 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Regular Exam - 15 Oct 2023/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Regular Exam - 15 Oct 2023/ExamSolutions.sql
@@ -135,3 +135,16 @@ SELECT
 	  [Bookings] AS b ON b.[TouristId] = t.[Id]
 WHERE b.[Id] IS NULL
 ORDER BY t.[Name]
+
+-- Problem 08
+SELECT TOP 10
+    h.[Name] AS [HotelName],
+	d.[Name] AS [DestinationName],
+	c.[Name] AS [CountryName]
+FROM [Bookings] AS b
+JOIN [Hotels] h ON b.[HotelId] = h.[Id]
+JOIN [Destinations] d ON h.[DestinationId] = d.[Id]
+JOIN [Countries] c ON d.[CountryId] = c.[Id]
+WHERE b.[ArrivalDate] < '2023-12-31' AND h.[Id] % 2 = 1
+ORDER BY c.[Name], 
+	     b.[ArrivalDate]
