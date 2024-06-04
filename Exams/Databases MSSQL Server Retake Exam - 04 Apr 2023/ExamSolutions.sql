2074da0f6bc9bfd8d92a80d74a3f4f22c5af70dd
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-04 15:34:49 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
@@ -149,3 +149,18 @@ ORDER BY [Amount] DESC,
    pc.[ProductId] IS NULL
  ORDER BY 
 	c.[Name]
+
+-- Problem 08
+
+	SELECT TOP(7)	
+		i.[Number],
+		i.[Amount],
+		c.[Name] AS [Client]
+	 FROM [Invoices] AS i
+	 JOIN [Clients] AS c ON c.[Id] = i.[ClientId]
+  WHERE i.[IssueDate] < '2023-01-01' AND i.[Currency] = 'EUR'
+			OR
+		i.[Amount] > 500.00 AND c.[NumberVAT] LIKE 'DE%'
+ ORDER BY 
+	    i.[Number],
+	    i.[Amount] DESC
