2ec37cfa26bb54713e5f7637fa221fe7753194f6
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-04 14:52:01 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
@@ -119,3 +119,17 @@ WHERE [NumberVAT] LIKE 'IT%'
 	  FROM [Invoices]
 ORDER BY [Amount] DESC,
 		     [DueDate] 
+
+-- Problem 06
+
+ SELECT 
+	  p.[Id],
+	  p.[Name],
+	  p.[Price],
+	  c.[Name] AS [CategoryName]
+   FROM [Products] AS p
+   JOIN [Categories] AS c ON c.[Id] = p.[CategoryId]
+  WHERE 
+	  c.[Name] = 'ADR' OR c.[Name] = 'Others'
+  ORDER BY 
+	  p.[Price] DESC
