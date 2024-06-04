2f3217625fd093c7454ffe283e4d8dfb3d59cbc6
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-04 16:01:26 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
@@ -219,3 +219,20 @@ BEGIN
 	)
 	RETURN @result
 END
+
+-- Problem 12
+
+CREATE OR ALTER PROCEDURE [usp_SearchByCountry](@country VARCHAR(10))
+AS
+	SELECT
+		v.[Name] AS [Vendor],
+		v.[NumberVAT] AS [VAT],
+		CONCAT(a.[StreetName], ' ', [StreetNumber]) AS [Street Info],
+		CONCAT(a.[City], ' ', a.[PostCode]) AS [City Info]
+	FROM [Vendors] AS v
+	JOIN [Addresses] AS a ON a.[Id] = v.[AddressId]
+	JOIN [Countries] AS c ON c.[Id] = a.[CountryId]
+   WHERE 
+	   c.[Name] = @country
+ORDER BY 
+	   v.[Name], a.[City]
