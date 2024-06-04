8606ef760f2582d54bd4dbcd4460d0a413b4c741
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-04 15:43:11 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 04 Apr 2023/ExamSolutions.sql
@@ -164,3 +164,20 @@ ORDER BY [Amount] DESC,
  ORDER BY 
 	    i.[Number],
 	    i.[Amount] DESC
+
+-- Problem 09
+
+   SELECT 
+		c.[Name] AS [Client],
+	MAX(p.[Price]) AS [Price],
+	    c.[NumberVAT] AS [VAT Number]
+     FROM [Clients] AS c
+LEFT JOIN 
+	      [ProductsClients] AS pc ON pc.[ClientId] = c.[Id]
+	 JOIN [Products] AS p ON p.[Id] = pc.ProductId
+	WHERE 
+		c.[Name] NOT LIKE '%KG'
+ GROUP BY 
+	    c.[Name], c.[NumberVAT]
+ ORDER BY 
+	MAX(p.[Price]) DESC
