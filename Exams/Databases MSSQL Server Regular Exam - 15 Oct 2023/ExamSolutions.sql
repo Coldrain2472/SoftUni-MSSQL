92eef3c7df8ede112b509af74da0a2215e182c31
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-04 13:38:43 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Regular Exam - 15 Oct 2023/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Regular Exam - 15 Oct 2023/ExamSolutions.sql
@@ -124,3 +124,14 @@ ORDER BY
 	   h.[Id], h.[Name]
    ORDER BY
 		COUNT(b.[Id]) DESC
+
+-- Problem 07
+SELECT 
+	t.[Id],
+	t.[Name],
+	t.[PhoneNumber]
+ FROM [Tourists] AS t
+ LEFT JOIN 
+	  [Bookings] AS b ON b.[TouristId] = t.[Id]
+WHERE b.[Id] IS NULL
+ORDER BY t.[Name]
