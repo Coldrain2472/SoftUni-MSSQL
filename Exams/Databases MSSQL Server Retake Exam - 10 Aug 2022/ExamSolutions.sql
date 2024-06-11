278406b1b704be6a54cfff1f796881de2fc856a6
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-11 13:44:23 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 10 Aug 2022/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 10 Aug 2022/ExamSolutions.sql
@@ -156,3 +156,21 @@ ORDER BY
    AND s.[Establishment] LIKE '%BC'
 ORDER BY
        s.[Name]
+
+-- Problem 09
+
+   SELECT 
+	    t.[Name],
+	    t.[Age],
+	    t.[PhoneNumber],
+	    t.[Nationality],
+	   CASE
+		   WHEN bp.[Name] IS NULL 
+		   THEN '(no bonus prize)'
+	   ELSE bp.[Name]
+	   END AS 'Reward'
+     FROM [Tourists] AS t
+LEFT JOIN [TouristsBonusPrizes] AS tbp ON tbp.[TouristId] = t.[Id]
+LEFT JOIN [BonusPrizes] AS bp ON bp.[Id] = tbp.[BonusPrizeId]
+ ORDER BY 
+	    t.[Name]
