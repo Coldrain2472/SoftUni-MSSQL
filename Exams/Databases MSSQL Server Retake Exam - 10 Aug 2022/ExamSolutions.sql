bb5332e056115ca41f636d1ecbf87a919baea24e
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-11 13:27:47 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 10 Aug 2022/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 10 Aug 2022/ExamSolutions.sql
@@ -139,3 +139,20 @@ ORDER BY
 	     [Sites] DESC,
 	     [Location]
 
+-- Problem 08
+
+  SELECT 
+	   s.[Name] AS [Site],
+       l.[Name] AS [Location],
+       l.[Municipality],
+       l.[Province],
+       s.[Establishment]
+    FROM [Sites] AS s
+    JOIN [Locations] AS l ON l.[Id] = s.[LocationId]
+   WHERE 
+	   l.[Name] NOT LIKE 'B%' 
+	AND l.[Name] NOT LIKE 'M%' 
+   AND l.[Name] NOT LIKE 'D%' 
+   AND s.[Establishment] LIKE '%BC'
+ORDER BY
+       s.[Name]
