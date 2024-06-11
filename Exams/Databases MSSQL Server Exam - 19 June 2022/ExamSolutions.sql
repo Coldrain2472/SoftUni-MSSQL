ba7ef0113952a1ab84518a6fd6e9e52822688e65
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-11 10:52:04 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Exam - 19 June 2022/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Exam - 19 June 2022/ExamSolutions.sql
@@ -143,3 +143,16 @@ ORDER BY [CountOfAnimals] DESC,
  ORDER BY 
 	   o.[Name],
 	   a.[Name] DESC
+
+-- Problem 09
+
+  SELECT 
+	   v.[Name],
+	   v.[PhoneNumber],
+	   SUBSTRING(v.[Address], CHARINDEX(',', v.[Address]) + 2, LEN(v.[Address])) AS [Address]
+	FROM [Volunteers] AS v
+	JOIN [VolunteersDepartments] AS vd ON vd.[Id] = v.[DepartmentId]
+   WHERE 
+	  vd.[DepartmentName] = 'Education program assistant' AND v.[Address] LIKE '%Sofia%'
+ORDER BY 
+	   v.[Name]
