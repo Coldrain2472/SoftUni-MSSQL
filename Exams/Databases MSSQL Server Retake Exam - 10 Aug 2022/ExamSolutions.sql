adc39f25e9219dcebd4a9abba958814f4ca2ee2e
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-11 13:10:41 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 10 Aug 2022/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 10 Aug 2022/ExamSolutions.sql
@@ -106,11 +106,12 @@ ORDER BY
 	[Name]
 
 -- Problem 06
+
   SELECT 
-	   s.[Name],
-	   l.[Name],
+	   s.[Name] AS [Site],
+	   l.[Name] AS [Location],
 	   s.[Establishment],
-	   c.[Name]
+	   c.[Name] AS [Category]
     FROM [Sites] AS s
 	JOIN [Locations] AS l ON l.[Id] = s.[LocationId]
 	JOIN [Categories] AS c ON c.[Id] = s.[CategoryId]
