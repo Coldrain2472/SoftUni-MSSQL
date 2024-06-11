4e605508de82d3870b6f9c3add908a2d6ffe7548
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-11 11:04:35 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Exam - 19 June 2022/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Exam - 19 June 2022/ExamSolutions.sql
@@ -186,3 +186,23 @@ BEGIN
 		GROUP BY vd.[DepartmentName]
 		)
 END
+
+-- Problem 12
+
+CREATE PROCEDURE [usp_AnimalsWithOwnersOrNot](@AnimalName VARCHAR(30)) 
+AS
+BEGIN
+		IF (SELECT [OwnerId] FROM [Animals] WHERE [Name] = @AnimalName) IS NULL
+		BEGIN 
+			SELECT [Name], 'For adoption' AS [OwnerName]
+			FROM [Animals]
+			WHERE [Name] = @AnimalName
+		END
+	ELSE
+		BEGIN
+			SELECT a.[Name], o.[Name] AS [OwnerName]
+				FROM [Animals] AS a
+				JOIN [Owners] AS o ON o.[Id] = a.[OwnerId]
+				WHERE a.[Name] = @AnimalName
+		END
+END
