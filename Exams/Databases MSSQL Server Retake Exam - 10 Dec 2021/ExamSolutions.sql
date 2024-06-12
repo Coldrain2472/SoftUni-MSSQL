fc1a9872f6a55359ece26888e47db36399c13daf
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-12 10:20:23 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 10 Dec 2021/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 10 Dec 2021/ExamSolutions.sql
@@ -137,3 +137,23 @@ WHERE
 ORDER BY 
     fd.[TicketPrice] DESC,
     a.[AirportName]
+
+-- Problem 08
+
+SELECT 
+    a.[Id] AS [AircraftId],
+    a.[Manufacturer],
+    a.[FlightHours],
+    COUNT(fd.[Id]) AS [FlightDestinationsCount],
+    ROUND(AVG(fd.[TicketPrice]), 2) AS [AvgPrice]
+FROM 
+    [Aircraft] AS a
+JOIN 
+    [FlightDestinations] AS fd ON a.[Id] = fd.[AircraftId]
+GROUP BY 
+    a.[Id], a.[Manufacturer], a.[FlightHours]
+HAVING 
+    COUNT(fd.[Id]) >= 2
+ORDER BY 
+    [FlightDestinationsCount] DESC,
+    a.[Id]
