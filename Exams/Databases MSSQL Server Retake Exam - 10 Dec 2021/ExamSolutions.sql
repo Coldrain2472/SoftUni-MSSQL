51ab7638e966fd43ffe0c0249f62d4fefc19ebca
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-12 10:16:19 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 10 Dec 2021/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 10 Dec 2021/ExamSolutions.sql
@@ -97,3 +97,23 @@ FROM
     [Aircraft]
 ORDER BY 
     [FlightHours] DESC
+
+-- Problem 06
+
+SELECT 
+    p.[FirstName],
+    p.[LastName],
+    a.[Manufacturer],
+    a.[Model],
+    a.[FlightHours]
+FROM 
+    [Pilots] AS p
+JOIN 
+    [PilotsAircraft] AS pa ON p.[Id] = pa.[PilotId]
+JOIN 
+    [Aircraft] AS a ON pa.[AircraftId] = a.[Id]
+WHERE 
+    a.[FlightHours] < 304 AND a.[FlightHours] IS NOT NULL
+ORDER BY 
+    a.[FlightHours] DESC,
+    p.[FirstName]
