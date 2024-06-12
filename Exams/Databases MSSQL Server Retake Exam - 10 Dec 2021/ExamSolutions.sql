2548541818ab78dce8fa972ea27727a78558dc69
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-12 10:18:17 +0300

    Update ExamSolutions.sql

--- Exams/Databases MSSQL Server Retake Exam - 10 Dec 2021/ExamSolutions.sql
+++ Exams/Databases MSSQL Server Retake Exam - 10 Dec 2021/ExamSolutions.sql
@@ -117,3 +117,23 @@ WHERE
 ORDER BY 
     a.[FlightHours] DESC,
     p.[FirstName]
+
+-- Problem 07
+
+SELECT TOP(20)
+    fd.[Id] AS [DestinationId],
+    fd.[Start],
+    p.[FullName],
+    a.[AirportName],
+    fd.[TicketPrice]
+FROM 
+    [FlightDestinations] AS fd
+JOIN 
+    [Passengers] AS p ON fd.[PassengerId] = p.[Id]
+JOIN 
+    [Airports] AS a ON fd.[AirportId] = a.[Id]
+WHERE 
+    DAY(fd.[Start]) % 2 = 0
+ORDER BY 
+    fd.[TicketPrice] DESC,
+    a.[AirportName]
